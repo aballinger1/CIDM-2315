3cde049da75ee8f552a61629c2304392485c6345
aballinger1 <112543614+aballinger1@users.noreply.github.com>
2022-08-31 03:06:39 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //create varibles and excute 
+        double x = 2.5;
+        double y = 3.3;
+        double z = (4*x*x) + (3*y);
+        //print to screen
+        Console.WriteLine("X = " +x);
+        Console.WriteLine("Y = "+y);
+        Console.WriteLine("The Value of Z is: "+z);
+    }
+}
