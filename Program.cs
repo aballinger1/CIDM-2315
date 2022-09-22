dc22f016b42485c98228ff85d2bca6dbc935de73
aballinger1 <112543614+aballinger1@users.noreply.github.com>
2022-09-21 21:57:51 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,49 +1,32 @@
-﻿namespace Homework4Qust2;
+﻿namespace Homework4Qust1;
 class Program
 {
     static void Main(string[] args)
     {
-        int n; 
-        //User inputs
-        Console.WriteLine("CIDM 2315 Homework 4 question 2");
-        Console.WriteLine("User will input a Value NOW !!!");
-        n = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("User will pick right(1) or left(2) NOW !!!");
-        string cho1 = Console.ReadLine();
+            int a;
+            int b;
+            int max;
 
-        int c = 1, d =2;
-                
-        // if-else statement
-        if(c >= d) 
-        {
-            // left tringle 
-            for(int row = 0; row<n; row++)
-            {
-            for(int col = 0; col<n; col++)
-                {
-                if(row>=col)
-                Console.Write('*');
-                }
-            Console.WriteLine("");
-            }
-        }
-        else 
-        {        
-            //right triangle 
-            int i, j, k ;  
-            for (i = 1; i <= n; i++)  
-            {  
-            for (j = 1; j <= n-i; j++)  
-                {  
-               Console.Write(" ");  
-                }  
-            for (k = 1; k <= i; k++)  
-                {  
-               Console.Write("*");  
-                }  
-            Console.WriteLine("");  
-            }  
-        }
-        Console.WriteLine("You Are Awesome");   
+            //input numbers
+            Console.Write("Enter first number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            //finding max number
+            if (a > b)
+                max = a;
+            else
+                max = b;
+           
+            //finding max number
+            max = (a > b) ? a : b;
+
+            //printing
+            Console.WriteLine("A = {0}",a);
+            Console.WriteLine("B = {0}",b);
+            Console.WriteLine("Maximum number = {0}", max);
+
+           
     }
 }
